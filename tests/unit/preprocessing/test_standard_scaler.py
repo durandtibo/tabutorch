dd9c14bdb95865c4924636d8ac3ba0d41d110398
Thibaut Durand <durand.tibo@gmail.com>
2024-12-16 13:13:46 -0800

    Improve `StandardScaler` tests

--- tests/unit/preprocessing/test_standard_scaler.py
+++ tests/unit/preprocessing/test_standard_scaler.py
@@ -11,9 +11,8 @@ from torch.nn.functional import mse_loss
 from tabutorch.preprocessing import StandardScaler
 
 SIZES = [1, 2, 3]
+SHAPES = [(2, 4), (2, 3, 4), (2, 3, 3, 4)]
 
-# TODO (tibo): behavior for batch size == 1  # noqa: TD003
-# TODO (tibo): behavior when scale == 0 # noqa: TD003
 # TODO (tibo): behavior when NaN values # noqa: TD003
 
 ####################################
@@ -33,33 +32,31 @@ def test_standard_scaler_init_feature_size(feature_size: int) -> None:
 
 
 @pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
 @pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_forward_2d(
-    device: str, batch_size: int, feature_size: int, mode: bool
-) -> None:
+def test_standard_scaler_forward(device: str, mode: bool) -> None:
     device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
+    module = StandardScaler(num_features=3).to(device=device)
+    module.load_state_dict(
+        {"mean": torch.tensor([4.0, 2.0, 1.0]), "scale": torch.tensor([1.0, 2.0, 4.0])}
+    )
     module.train(mode)
-    x = torch.randn(batch_size, feature_size, device=device)
-    out = module(x)
-    assert objects_are_equal(out, x)
+    out = module(torch.tensor([[1.0, 2.0, 3.0], [1.0, 1.0, 1.0], [2.0, 2.0, 2.0]], device=device))
+    assert objects_are_allclose(
+        out,
+        torch.tensor([[-3.0, 0.0, 0.5], [-3.0, -0.5, 0.0], [-2.0, 0.0, 0.25]], device=device),
+    )
 
 
 @pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("d1", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
+@pytest.mark.parametrize("shape", SHAPES)
 @pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_forward_3d(
-    device: str, batch_size: int, d1: int, feature_size: int, mode: bool
-) -> None:
+def test_standard_scaler_forward_shape(device: str, shape: tuple[int, ...], mode: bool) -> None:
     device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
+    module = StandardScaler(num_features=4).to(device=device)
     module.train(mode)
-    x = torch.randn(batch_size, d1, feature_size, device=device)
+    x = torch.randn(*shape, device=device)
     out = module(x)
+    assert x is not out
     assert objects_are_equal(out, x)
 
 
@@ -78,52 +75,16 @@ def test_standard_scaler_backward(device: str, batch_size: int, mode: bool) -> N
 
 
 @pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
 @pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_fit_2d(
-    device: str, batch_size: int, feature_size: int, mode: bool
-) -> None:
-    device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
-    module.train(mode)
-    x = torch.randn(batch_size, feature_size, device=device)
-    module.fit(x)
-    out = module.transform(x)
-    assert out.shape == (batch_size, feature_size)
-    assert out.device == device
-    assert out.dtype == torch.float
-
-
-@pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("d1", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
-@pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_fit_3d(
-    device: str, batch_size: int, d1: int, feature_size: int, mode: bool
-) -> None:
-    device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
-    module.train(mode)
-    x = torch.randn(batch_size, d1, feature_size, device=device)
-    module.fit(x)
-    out = module.transform(x)
-    assert out.shape == (batch_size, d1, feature_size)
-    assert out.device == device
-    assert out.dtype == torch.float
-
-
-@pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_fit_custom(device: str, mode: bool) -> None:
+def test_standard_scaler_fit(device: str, mode: bool) -> None:
     device = torch.device(device)
     module = StandardScaler(num_features=4).to(device=device)
     module.train(mode)
-    x = torch.tensor(
-        [[4.0, 3.0, 4.0, 1.0], [0.0, 1.0, 3.0, 3.0], [2.0, 2.0, 2.0, 2.0]], device=device
+    module.fit(
+        torch.tensor(
+            [[4.0, 3.0, 4.0, 1.0], [0.0, 1.0, 3.0, 3.0], [2.0, 2.0, 2.0, 2.0]], device=device
+        )
     )
-    module.fit(x)
     assert objects_are_allclose(
         module.state_dict(),
         OrderedDict(
@@ -133,6 +94,21 @@ def test_standard_scaler_fit_custom(device: str, mode: bool) -> None:
             }
         ),
     )
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("shape", SHAPES)
+@pytest.mark.parametrize("mode", [True, False])
+def test_standard_scaler_fit_shape(device: str, shape: tuple[int, ...], mode: bool) -> None:
+    device = torch.device(device)
+    module = StandardScaler(num_features=4).to(device=device)
+    module.train(mode)
+    x = torch.randn(*shape, device=device)
+    module.fit(x)
+    out = module.transform(x)
+    assert out.shape == shape
+    assert out.device == device
+    assert out.dtype == torch.float
 
 
 @pytest.mark.parametrize("device", get_available_devices())
@@ -154,50 +130,16 @@ def test_standard_scaler_fit_constant(device: str, mode: bool) -> None:
 
 
 @pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
 @pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_fit_transform_2d(
-    device: str, batch_size: int, feature_size: int, mode: bool
-) -> None:
-    device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
-    module.train(mode)
-    x = torch.randn(batch_size, feature_size, device=device)
-    out = module.fit_transform(x)
-    assert out.shape == (batch_size, feature_size)
-    assert out.device == device
-    assert out.dtype == torch.float
-
-
-@pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("d1", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
-@pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_fit_transform_3d(
-    device: str, batch_size: int, d1: int, feature_size: int, mode: bool
-) -> None:
-    device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
-    module.train(mode)
-    x = torch.randn(batch_size, d1, feature_size, device=device)
-    out = module.fit_transform(x)
-    assert out.shape == (batch_size, d1, feature_size)
-    assert out.device == device
-    assert out.dtype == torch.float
-
-
-@pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_fit_transform_custom(device: str, mode: bool) -> None:
+def test_standard_scaler_fit_transform(device: str, mode: bool) -> None:
     device = torch.device(device)
     module = StandardScaler(num_features=4).to(device=device)
     module.train(mode)
-    x = torch.tensor(
-        [[4.0, 3.0, 4.0, 1.0], [0.0, 1.0, 3.0, 3.0], [2.0, 2.0, 2.0, 2.0]], device=device
+    out = module.fit_transform(
+        torch.tensor(
+            [[4.0, 3.0, 4.0, 1.0], [0.0, 1.0, 3.0, 3.0], [2.0, 2.0, 2.0, 2.0]], device=device
+        )
     )
-    out = module.fit_transform(x)
     assert objects_are_allclose(
         module.state_dict(),
         OrderedDict(
@@ -213,6 +155,22 @@ def test_standard_scaler_fit_transform_custom(device: str, mode: bool) -> None:
             [[1.0, 1.0, 1.0, -1.0], [-1.0, -1.0, 0.0, 1.0], [0.0, 0.0, -1.0, 0.0]], device=device
         ),
     )
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("shape", SHAPES)
+@pytest.mark.parametrize("mode", [True, False])
+def test_standard_scaler_fit_transform_shape(
+    device: str, shape: tuple[int, ...], mode: bool
+) -> None:
+    device = torch.device(device)
+    module = StandardScaler(num_features=4).to(device=device)
+    module.train(mode)
+    x = torch.randn(*shape, device=device)
+    out = module.fit_transform(x)
+    assert out.shape == shape
+    assert out.device == device
+    assert out.dtype == torch.float
 
 
 @pytest.mark.parametrize("device", get_available_devices())
@@ -235,54 +193,34 @@ def test_standard_scaler_fit_transform_constant(device: str, mode: bool) -> None
 
 
 @pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
 @pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_transform_2d(
-    device: str, batch_size: int, feature_size: int, mode: bool
-) -> None:
+def test_standard_scaler_transform(device: str, mode: bool) -> None:
     device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
-    module.train(mode)
-    x = torch.randn(batch_size, feature_size, device=device)
-    out = module.transform(x)
-    assert objects_are_equal(out, x)
-
-
-@pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("batch_size", SIZES)
-@pytest.mark.parametrize("d1", SIZES)
-@pytest.mark.parametrize("feature_size", SIZES)
-@pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_transform_3d(
-    device: str, batch_size: int, d1: int, feature_size: int, mode: bool
-) -> None:
-    device = torch.device(device)
-    module = StandardScaler(num_features=feature_size).to(device=device)
-    module.train(mode)
-    x = torch.randn(batch_size, d1, feature_size, device=device)
-    out = module.transform(x)
-    assert objects_are_equal(out, x)
-
-
-@pytest.mark.parametrize("device", get_available_devices())
-@pytest.mark.parametrize("mode", [True, False])
-def test_standard_scaler_transform_custom(device: str, mode: bool) -> None:
-    device = torch.device(device)
-    module = StandardScaler(num_features=4).to(device=device)
+    module = StandardScaler(num_features=3).to(device=device)
     module.load_state_dict(
-        {"mean": torch.tensor([1, 2, 3, 4]), "scale": torch.tensor([0.1, 0.2, 0.3, 0.4])}
+        {"mean": torch.tensor([4.0, 2.0, 1.0]), "scale": torch.tensor([1.0, 2.0, 4.0])}
     )
     module.train(mode)
     out = module.transform(
-        torch.tensor([[4.0, 3.0, 2.0, 1.0], [0.0, 1.0, 2.0, 3.0]], device=device)
+        torch.tensor([[1.0, 2.0, 3.0], [1.0, 1.0, 1.0], [2.0, 2.0, 2.0]], device=device)
     )
     assert objects_are_allclose(
         out,
-        torch.tensor(
-            [[30.0, 5.0, -10.0 / 3.0, -7.5], [-10.0, -5.0, -10.0 / 3.0, -2.5]], device=device
-        ),
+        torch.tensor([[-3.0, 0.0, 0.5], [-3.0, -0.5, 0.0], [-2.0, 0.0, 0.25]], device=device),
     )
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("shape", SHAPES)
+@pytest.mark.parametrize("mode", [True, False])
+def test_standard_scaler_transform_shape(device: str, shape: tuple[int, ...], mode: bool) -> None:
+    device = torch.device(device)
+    module = StandardScaler(num_features=4).to(device=device)
+    module.train(mode)
+    x = torch.randn(*shape, device=device)
+    out = module.transform(x)
+    assert x is not out
+    assert objects_are_equal(out, x)
 
 
 def test_standard_scaler_load_state_dict() -> None:

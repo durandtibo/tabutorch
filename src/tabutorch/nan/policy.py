1aeccc2d6f347d3dd507ae1b9b63fdecbe6a2137
Thibaut Durand <durand.tibo@gmail.com>
2024-12-16 14:29:57 -0800

    Refactor code (#20)

--- src/tabutorch/nan/policy.py
+++ src/tabutorch/nan/policy.py
@@ -2,9 +2,9 @@ r"""Contain utility functions to manage tensor with NaN values."""
 
 from __future__ import annotations
 
-__all__ = ["check_nan_policy", "contains_nan", "mean"]
+__all__ = ["check_nan_policy", "contains_nan"]
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     import torch
@@ -24,7 +24,7 @@ def check_nan_policy(nan_policy: str) -> None:
 
     ```pycon
 
-    >>> from tabutorch.utils.nan import check_nan_policy
+    >>> from tabutorch.nan import check_nan_policy
     >>> check_nan_policy(nan_policy="omit")
 
     ```
@@ -61,7 +61,7 @@ def contains_nan(
     ```pycon
 
     >>> import torch
-    >>> from tabutorch.utils.nan import contains_nan
+    >>> from tabutorch.nan import contains_nan
     >>> contains_nan(torch.tensor([1.0, 2.0, 3.0]))
     False
     >>> contains_nan(torch.tensor([1.0, 2.0, float("nan")]))
@@ -75,45 +75,3 @@ def contains_nan(
         msg = f"{name} contains at least one NaN value"
         raise ValueError(msg)
     return isnan
-
-
-def mean(x: torch.Tensor, *args: Any, nan_policy: str = "propagate", **kwargs: Any) -> torch.Tensor:
-    r"""Return the mean values.
-
-    Args:
-        x: The input tensor.
-        *args: Positional arguments of ``torch.mean`` or
-            ``torch.nanmean``.
-        nan_policy: The policy on how to handle NaN values in the input
-            tensor when estimating the mean. The following options are
-            available: ``'omit'`` and ``'propagate'``.
-        **kwargs: Keyword arguments of ``torch.mean`` or
-            ``torch.nanmean``.
-
-    Returns:
-        Returns the mean values.
-
-    Raises:
-        ValueError: if the ``nan_policy`` value is incorrect.
-
-    Example usage:
-
-    ```pycon
-
-    >>> import torch
-    >>> from tabutorch.utils.nan import mean
-    >>> mean(torch.tensor([1.0, 2.0, 3.0]))
-    tensor(2.)
-    >>> mean(torch.tensor([1.0, 2.0, float("nan")]))
-    tensor(nan)
-    >>> mean(torch.tensor([1.0, 2.0, float("nan")]), nan_policy="omit")
-    tensor(1.5000)
-
-    ```
-    """
-    if nan_policy == "propagate":
-        return x.mean(*args, **kwargs)
-    if nan_policy == "omit":
-        return x.nanmean(*args, **kwargs)
-    msg = f"Incorrect 'nan_policy': {nan_policy}. The valid values are: 'omit' and 'propagate'"
-    raise ValueError(msg)

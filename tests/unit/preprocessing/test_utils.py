b5bdd79eb3586d1ea83bcb58dab77d7c255ebc1c
Thibaut Durand <durand.tibo@gmail.com>
2024-12-16 11:20:19 -0800

    Add `handle_zeros_in_scale`

--- tests/unit/preprocessing/test_utils.py
+++ tests/unit/preprocessing/test_utils.py
@@ -3,7 +3,52 @@ from __future__ import annotations
 import torch
 from coola import objects_are_equal
 
-from tabutorch.preprocessing.utils import is_constant_feature
+from tabutorch.preprocessing.utils import handle_zeros_in_scale, is_constant_feature
+
+###########################################
+#     Tests for handle_zeros_in_scale     #
+###########################################
+
+
+def test_handle_zeros_in_scale_1d() -> None:
+    assert objects_are_equal(
+        handle_zeros_in_scale(scale=torch.tensor([4.0, 3.0, 2.0, 1.0, 0.0])),
+        torch.tensor([4.0, 3.0, 2.0, 1.0, 1.0]),
+    )
+
+
+def test_handle_zeros_in_scale_2d() -> None:
+    assert objects_are_equal(
+        handle_zeros_in_scale(scale=torch.tensor([[4.0, 3.0, 2.0], [1.0, 0.0, 1e-9]])),
+        torch.tensor([[4.0, 3.0, 2.0], [1.0, 1.0, 1.0]]),
+    )
+
+
+def test_handle_zeros_in_scale_copy_true() -> None:
+    scale = torch.tensor([4.0, 3.0, 2.0, 1.0, 0.0])
+    out = handle_zeros_in_scale(scale)
+    assert scale is not out
+    assert objects_are_equal(scale, torch.tensor([4.0, 3.0, 2.0, 1.0, 0.0]))
+    assert objects_are_equal(out, torch.tensor([4.0, 3.0, 2.0, 1.0, 1.0]))
+
+
+def test_handle_zeros_in_scale_copy_false() -> None:
+    scale = torch.tensor([4.0, 3.0, 2.0, 1.0, 0.0])
+    out = handle_zeros_in_scale(scale, copy=False)
+    assert scale is out
+    assert objects_are_equal(scale, torch.tensor([4.0, 3.0, 2.0, 1.0, 1.0]))
+    assert objects_are_equal(out, torch.tensor([4.0, 3.0, 2.0, 1.0, 1.0]))
+
+
+def test_handle_zeros_in_scale_constant_mask() -> None:
+    assert objects_are_equal(
+        handle_zeros_in_scale(
+            scale=torch.tensor([4.0, 1e-10, 2.0, 1.0, 0.0]),
+            constant_mask=torch.tensor([False, True, False, False, True]),
+        ),
+        torch.tensor([4.0, 1.0, 2.0, 1.0, 1.0]),
+    )
+
 
 #########################################
 #     Tests for is_constant_feature     #

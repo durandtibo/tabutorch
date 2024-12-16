2da13ac14f133348522d130836c5e17c664e5f03
Thibaut Durand <durand.tibo@gmail.com>
2024-12-15 18:05:38 -0800

    Add `BaseTransformer`

---
+++ src/tabutorch/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain modules to prepare or transform data."""
+
+from __future__ import annotations
+
+__all__ = ["BaseTransformer"]
+
+from src.tabutorch.preprocessing.base import BaseTransformer

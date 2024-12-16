ab2ff21a8c5ac9a728f24d1b1b8f3d60dbe1bf93
Thibaut Durand <durand.tibo@gmail.com>
2024-12-16 14:51:40 -0800

    Add `std` (WIP)

--- src/tabutorch/nan/reduction.py
+++ src/tabutorch/nan/reduction.py
@@ -2,7 +2,7 @@ r"""Contain reduction functions to manage tensors with NaN values."""
 
 from __future__ import annotations
 
-__all__ = ["mean"]
+__all__ = ["mean", "std"]
 
 from typing import TYPE_CHECKING, Any
 
@@ -48,5 +48,47 @@ def mean(x: torch.Tensor, *args: Any, nan_policy: str = "propagate", **kwargs: A
         return x.mean(*args, **kwargs)
     if nan_policy == "omit":
         return x.nanmean(*args, **kwargs)
+    msg = f"Incorrect 'nan_policy': {nan_policy}. The valid values are: 'omit' and 'propagate'"
+    raise ValueError(msg)
+
+
+def std(x: torch.Tensor, *args: Any, correction=1, nan_policy: str = "propagate", **kwargs: Any) -> torch.Tensor:
+    r"""Return the standard deviation values.
+
+    Args:
+        x: The input tensor.
+        *args: Positional arguments of ``torch.std`` or
+            ``torch.nanstd``.
+        nan_policy: The policy on how to handle NaN values in the input
+            tensor when estimating the std. The following options are
+            available: ``'omit'`` and ``'propagate'``.
+        **kwargs: Keyword arguments of ``torch.std`` or
+            ``torch.nanstd``.
+
+    Returns:
+        Returns the standard deviation values.
+
+    Raises:
+        ValueError: if the ``nan_policy`` value is incorrect.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import torch
+    >>> from tabutorch.nan import std
+    >>> std(torch.tensor([1.0, 2.0, 3.0]))
+    tensor(2.)
+    >>> std(torch.tensor([1.0, 2.0, float("nan")]))
+    tensor(nan)
+    >>> std(torch.tensor([1.0, 2.0, float("nan")]), nan_policy="omit")
+    tensor(1.5000)
+
+    ```
+    """
+    if nan_policy == "propagate":
+        return x.std(*args, **kwargs)
+    if nan_policy == "omit":
+        return nanstd(x, *args, **kwargs)
     msg = f"Incorrect 'nan_policy': {nan_policy}. The valid values are: 'omit' and 'propagate'"
     raise ValueError(msg)

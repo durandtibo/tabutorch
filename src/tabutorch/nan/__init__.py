cb778d82c1937b43f2d373cfbe62229c58d42a12
Thibaut Durand <durand.tibo@gmail.com>
2024-12-16 16:52:21 -0800

    Add `nanstd` (#24)

--- src/tabutorch/nan/__init__.py
+++ src/tabutorch/nan/__init__.py
@@ -2,7 +2,7 @@ r"""Contain utility functions to manage tensor with NaN values."""
 
 from __future__ import annotations
 
-__all__ = ["check_nan_policy", "contains_nan", "mean", "nanvar"]
+__all__ = ["check_nan_policy", "contains_nan", "mean", "nanstd", "nanvar"]
 
 from tabutorch.nan.policy import check_nan_policy, contains_nan
-from tabutorch.nan.reduction import mean, nanvar
+from tabutorch.nan.reduction import mean, nanstd, nanvar

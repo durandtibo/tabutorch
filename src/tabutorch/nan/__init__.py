54f81460b353cc28b00992cadc435cc568e375a1
Thibaut Durand <durand.tibo@gmail.com>
2024-12-16 17:35:03 -0800

    Add `var` (#26)

--- src/tabutorch/nan/__init__.py
+++ src/tabutorch/nan/__init__.py
@@ -2,7 +2,15 @@ r"""Contain utility functions to manage tensor with NaN values."""
 
 from __future__ import annotations
 
-__all__ = ["check_nan_policy", "contains_nan", "mean", "nanstd", "nanvar", "std"]
+__all__ = [
+    "check_nan_policy",
+    "contains_nan",
+    "mean",
+    "nanstd",
+    "nanvar",
+    "std",
+    "var",
+]
 
 from tabutorch.nan.policy import check_nan_policy, contains_nan
-from tabutorch.nan.reduction import mean, nanstd, nanvar, std
+from tabutorch.nan.reduction import mean, nanstd, nanvar, std, var
